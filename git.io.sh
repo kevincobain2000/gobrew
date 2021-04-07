7f024882a696c20effb44b1c85164aa82ab0fa29
Pulkit Kathuria <kevincobain2000@gmail.com>
2021-04-07 10:56:48 +0900

    fixes: #4. POSIX compatible
    Former-commit-id: 4c0acc0b6d43b7ab535a46dae2c82993cf4a2d78

--- git.io.sh
+++ git.io.sh
@@ -1,23 +1,22 @@
-#! /bin/sh
+#!/bin/sh
 
 GOBREW_BIN_DIR=$HOME/.gobrew/bin
 mkdir -p $GOBREW_BIN_DIR
 
 GOBREW_ARCH_BIN=''
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        GOBREW_ARCH_BIN="gobrew-linux-64"
-elif [[ "$OSTYPE" == "linux"*  ]]; then
-        GOBREW_ARCH_BIN="gobrew-linux-64"        
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        GOBREW_ARCH_BIN="gobrew-darwin-64"
-elif [[ "$OSTYPE" == "win64" ]]; then
-        GOBREW_ARCH_BIN="gobrew-windows-64.exe"
-# elif [[ "$OSTYPE" == "freebsd"* ]]; then
-#         # ...
-# else
-#         # Unknown.
-fi
 
+THISOS=$(uname -s)
+case $THISOS in
+   Linux*)
+      GOBREW_ARCH_BIN="gobrew-linux-64"
+      ;;
+   Darwin*)
+      GOBREW_ARCH_BIN="gobrew-darwin-64"
+      ;;
+   Windows*)
+      GOBREW_ARCH_BIN="gobrew-windows-64.exe"
+      ;;
+esac
 
 curl -kLs https://raw.githubusercontent.com/kevincobain2000/gobrew/master/bin/$GOBREW_ARCH_BIN -o $GOBREW_BIN_DIR/gobrew
 
