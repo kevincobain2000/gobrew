ca1b082c838f40a189777124835c12e50e2d7798
Pulkit Kathuria <kevincobain2000@gmail.com>
2021-04-09 12:30:34 +0900

    oops
    Former-commit-id: 976dc7b019d39673f29820fae50c6a94de19e7e3

--- cmd/gobrew/main.go
+++ cmd/gobrew/main.go
@@ -90,7 +90,7 @@ func Find(slice []string, val string) (int, bool) {
 
 func usage() string {
 	msg := `
-gobrew 1.0.6
+gobrew 1.0.7
 
 Usage:
     gobrew help                         Show this message
