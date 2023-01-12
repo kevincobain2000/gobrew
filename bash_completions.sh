70345738a8200f6ba761beded47b5988776c3177
Pulkit Kathuria <kevincobain2000@gmail.com>
2023-01-13 00:38:57 +0900

    whoops

--- bash_completions.sh
+++ bash_completions.sh
@@ -6,7 +6,7 @@ _gobrew()
             COMMANDS="ls ls-remote use install uninstall prune version help"
             ;;
         2)
-            COMMANDS=`/Users/pulkit.kathuria/git/gobrew/main ls |sed '/*/d'| sed '/current/d' |awk NF`
+            COMMANDS=`gobrew ls |sed '/*/d'| sed '/current/d' |awk NF`
             ;;
     esac
     COMPREPLY=(`compgen -W "$COMMANDS" -- "${COMP_WORDS[COMP_CWORD]}"`)
