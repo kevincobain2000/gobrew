1c24883ac1fc06e9b07e80ea43a10c557042b31d
Evsyukov Denis <denis@evsyukov.org>
2023-12-08 14:35:27 +0300

    feat: cross build setup for main package

--- cmd/gobrew/main.go
+++ cmd/gobrew/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 	"os"
-	"runtime"
 	"strconv"
 	"strings"
 
@@ -167,23 +166,7 @@ Examples:
     gobrew use dev-latest          # use go version latest avalable, including rc and beta
 
 Installation Path:
-`
-
-	if runtime.GOOS == "windows" {
-		msg = msg + `
-    # Add gobrew to your environment variables
-    PATH="%USERPROFILE%\.gobrew\current\bin;%USERPROFILE%\.gobrew\bin;%PATH%"
-    GOROOT="%USERPROFILE%\.gobrew\current\go"
-
-`
-	} else {
-		msg = msg + `
-    # Add gobrew to your ~/.bashrc or ~/.zshrc
-    export PATH="$HOME/.gobrew/current/bin:$HOME/.gobrew/bin:$PATH"
-    export GOROOT="$HOME/.gobrew/current/go"
-
-`
-	}
+` + usageMsg
 
 	return msg
 }
