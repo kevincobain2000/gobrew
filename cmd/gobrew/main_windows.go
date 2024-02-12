486f5c830b2e57b161eecbd2fbe24237c0bb154d
Evsyukov Denis <denis@evsyukov.org>
2024-02-12 20:48:24 +0300

    fix: help message for windows

--- cmd/gobrew/main_windows.go
+++ cmd/gobrew/main_windows.go
@@ -3,8 +3,8 @@
 package main
 
 const usageMsg = `
-    # Add gobrew to your ~/.bashrc or ~/.zshrc
-    export PATH="$HOME/.gobrew/current/bin:$HOME/.gobrew/bin:$PATH"
-    export GOROOT="$HOME/.gobrew/current/go"
+    # Add gobrew to your environment variables
+    PATH="%USERPROFILE%\.gobrew\current\bin;%USERPROFILE%\.gobrew\bin;%PATH%"
+    GOROOT="%USERPROFILE%\.gobrew\current\go"
 
 `
